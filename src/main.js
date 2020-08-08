9561a9d301da01999b701194ded4965c84b88ef1
Deepali <iamdeepalikhaitan@gmail.com>
2020-08-09 02:36:05 +0530

    initialsetup

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './main.css';
+import filter from './filter'
+
+
+class mainpart extends React.Component{
+  constructor(props){
+      super(props);
+     this.state={
+
+    }
+      
+  }
+  
+
+  render()
+  { console.log("clicked year",this.props)
+
+    return(
+        <div className="container_main">
+          dataaaaaaa...
+
+        </div>
+    )
+  }
+ 
+
+}
+
+export default mainpart
+
